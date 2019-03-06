ab7d0b0481dec3ee9bf9772edbdd6d55294ae3bb
Dave 'Sri' Seah <david@davidseah.com>
2019-03-06 15:29:11 -0500

    dev-ds/data-modeling: prototype curve drawing and dragging

--- src/app-web/components/SVGView.jsx
+++ src/app-web/components/SVGView.jsx
@@ -8,13 +8,15 @@ should be a child of RoutedView
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import React from 'react';
 import PropTypes from 'prop-types';
-import SVG from 'svg.js';
+import SVG from '@svgdotjs/svg.js/src/svg';
+import '@svgdotjs/svg.draggable.js';
 
 const DBG = false;
 
 /// CLASS DECLARATION /////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 class SVGView extends React.Component {
+  //
   constructor(props) {
     super(props);
     //
@@ -50,7 +52,7 @@ class SVGView extends React.Component {
           `%c${prompt} props ${this.props.viewWidth} ${this.props.viewHeight}`,
           `color:blue`
         );
-      this.DrawTestScene(this.props.viewWidth, this.props.viewHeight);
+      this.DrawSystemDiagram(this.props.viewWidth, this.props.viewHeight);
     }
   }
 
@@ -66,6 +68,89 @@ class SVGView extends React.Component {
     this.Draw.clear();
     const rect = this.Draw.rect(ww, hh).attr({ fill: '#f06' });
     rect.move(pad, pad);
+  }
+
+  DrawSystemDiagram(w, h) {
+    const width = w || this.props.viewWidth;
+    const height = h || this.props.viewHeight;
+    // clear screen then drawnpm
+    this.Draw.clear();
+    console.log(`%cDrawSystemDiagram() ${width} ${height}`, 'color:green');
+    const xx = 100;
+    const yy = 100;
+    const ww = 200;
+    const hh = 100;
+    const pad = 10;
+    const Draw = this.Draw;
+    const COL_BG = '#F06';
+    // define symbols
+
+    function makePropElement(name = '<unknown>') {
+      const prop = Draw.group();
+      const rect = prop
+        .rect(10, 10)
+        .fill(COL_BG)
+        .radius(5);
+      const text = prop
+        .text(add => {
+          add.tspan(name);
+        })
+        .font({ weight: 'bold' })
+        .move(10, 5);
+      // resize rect to size of text
+      rect.size(Math.min(text.bbox().w, ww) + 20, Math.min(text.bbox().h, hh) + 10);
+      // play with events
+      prop.draggable();
+      prop.on('dragmove.propmove', event => {
+        const { handler, box } = event.detail;
+        event.preventDefault();
+        const { x, y } = box;
+        handler.move(x, y);
+        updatePath();
+      });
+      return prop;
+    }
+    // draw symbols
+    let p1 = { x: xx, y: yy };
+    let p2 = { x: xx + 2 * ww + pad, y: yy + pad + hh };
+    const S1 = makePropElement()
+      .cx(p1.x)
+      .cy(p1.y);
+    const S2 = makePropElement('Figaro')
+      .cx(p2.x)
+      .cy(p2.y);
+    // draw bezier
+    const up = 150;
+    const mech = Draw.path(
+      `M${p1.x},${p1.y} C${p1.x},${p1.y - up} ${p2.x},${p2.y - up} ${p2.x},${p2.y}`
+    )
+      .back()
+      .fill('none')
+      .stroke({ width: 4, color: 'orange' });
+    // draw label
+    const label = Draw.text(add => {
+      add.tspan('mechanism');
+    });
+    label
+      .fill('orange')
+      .attr('dy', -6)
+      .path(mech)
+      .attr('side', 'right')
+      .attr('startOffset', '50%')
+      .attr('textAnchor', 'middle');
+
+    function updatePath() {
+      p1.x = S1.cx();
+      p1.y = S1.cy();
+      p2.x = S2.cx();
+      p2.y = S2.cy();
+      if (p1.x > p2.x) {
+        const t = p1;
+        p1 = p2;
+        p2 = t;
+      }
+      mech.plot(`M${p1.x},${p1.y} C${p1.x},${p1.y - up} ${p2.x},${p2.y - up} ${p2.x},${p2.y}`);
+    }
   }
 
   render() {

dee9dd39ef85d7f62f9b8e145859dee8feb49174
Ben Loh <ben@inquirium.net>
2019-07-15 10:48:01 -0700

    dev-bl/classroom-admin-criteria-edit: Add CriteriaView modal dialog for projection by teachers during edit.

--- src/app-web/ViewAdmin/components/AdmCriteriaList.jsx
+++ src/app-web/ViewAdmin/components/AdmCriteriaList.jsx
@@ -1,6 +1,6 @@
 /*///////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
 
-Criteria List View
+Criteria List
 
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
 
@@ -8,10 +8,7 @@ Criteria List View
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -19,7 +16,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TextField from '@material-ui/core/TextField';
 // Material UI Icons
-import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 // Material UI Theming
 import { withStyles } from '@material-ui/core/styles';
@@ -28,7 +24,6 @@ import { withStyles } from '@material-ui/core/styles';
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import MEMEStyles from '../../components/MEMEStyles';
 import UR from '../../../system/ursys';
-import ADM from '../../modules/adm-data';
 
 /// DECLARATIONS //////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -39,196 +34,57 @@ const PKG = 'AdminCriteriaList';
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 class CriteriaList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.CloneCriteria = this.CloneCriteria.bind(this);
-    this.DoClassroomSelect = this.DoClassroomSelect.bind(this);
-    this.OnEditCriteriaClick = this.OnEditCriteriaClick.bind(this);
-    this.OnEditCriteriaSave = this.OnEditCriteriaSave.bind(this);
-    this.OnEditCriteriaCancel = this.OnEditCriteriaCancel.bind(this);
-    this.OnEditCriteriaClose = this.OnEditCriteriaClose.bind(this);
-    this.OnAddCriteriaClick = this.OnAddCriteriaClick.bind(this);
-    this.OnDeleteCriteriaClick = this.OnDeleteCriteriaClick.bind(this);
-    this.UpdateField = this.UpdateField.bind(this);
-
-    this.state = {
-      criteria: [],
-      isInEditMode: false,
-      classroomId: ''
-    };
-
-    UR.Sub('CLASSROOM_SELECT', this.DoClassroomSelect);
-  }
 
   componentDidMount() { }
 
   componentWillUnmount() { }
 
-  // Used to create a local copy for editing and support cancelling edit.
-  // NOTE: This is a shallow copy
-  CloneCriteria(criteria) {
-    return criteria.map(crit => {
-      return Object.assign({}, crit);
-    });
-  }
-
-  DoClassroomSelect(data) {
-    // Clone the criteria objects so that field updates do not change the original objects
-    let criteria = this.CloneCriteria(ADM.GetCriteriaByClassroom(data.classroomId));
-    this.setState({
-      criteria,
-      classroomId: data.classroomId
-    });
-  }
-
-  OnEditCriteriaClick() {
-    this.setState({
-      isInEditMode: true
-    });
-  }
-
-  OnEditCriteriaSave(e) {
-    ADM.UpdateCriteriaList(this.state.criteria);
-    this.OnEditCriteriaClose();
-  }
-
-  OnEditCriteriaCancel() {
-    // Restore original values.
-    this.DoClassroomSelect({ classroomId: this.state.classroomId });
-    this.OnEditCriteriaClose();
-  }
-
-  OnEditCriteriaClose() {
-    this.setState({
-      isInEditMode: false
-    });
-  }
-
-  OnAddCriteriaClick() {
-    this.setState(state => {
-      let criteria = state.criteria;
-      criteria.push(ADM.NewCriteria());
-      return {
-        criteria,
-        isInEditMode: true
-      };
-    });
-  }
-
-  OnDeleteCriteriaClick(critId) {
-    this.setState(state => {
-      const criteria = state.criteria;
-      const result = criteria.filter(crit => crit.id !== critId);
-      return {
-        criteria: result
-      };
-    });
-  }
-
-  UpdateField(critId, fieldName, value) {
-    // Save the changes locally first
-    // Store the whole object when "Save" is presssed.
-    this.setState(state => {
-      let criteria = state.criteria;
-
-      const i = criteria.findIndex(cr => cr.id === critId);
-      if (i < 0) {
-        console.error(PKG, 'UpdateField could not find index of criteria with id', critId);
-        return;
-      }
-
-      // Update the value
-      let crit = criteria[i];
-      crit[fieldName] = value;
-
-      // Update criteria data
-      criteria.splice(i, 1, crit);
-      return { criteria };
-    });
-  }
-
   render() {
-    const { classes } = this.props;
-    const { criteria, isInEditMode, classroomId } = this.state;
+    const { classes, Criteria, IsInEditMode, UpdateField, OnDeleteCriteriaClick } = this.props;
 
     return (
-      <Paper className={classes.admPaper}>
-        <InputLabel>CRITERIA</InputLabel>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>LABEL</TableCell>
-              <TableCell>DESCRIPTION</TableCell>
-              <TableCell>{isInEditMode ? 'DELETE' : ''}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {criteria.map(crit => (
-              isInEditMode ? (
-                <TableRow key={crit.id}>
-                  <TableCell>
-                    <TextField
-                      value={crit.label}
-                      placeholder="Label"
-                      onChange={e => this.UpdateField(crit.id, 'label', e.target.value)}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <TextField
-                      value={crit.description}
-                      placeholder="Description"
-                      onChange={e => this.UpdateField(crit.id, 'description', e.target.value)}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <IconButton size="small" onClick={() => this.OnDeleteCriteriaClick(crit.id)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ) : (
-                <TableRow key={crit.id}>
-                  <TableCell>{crit.label}</TableCell>
-                  <TableCell>{crit.description}</TableCell>
-                </TableRow>
-              )
-            ))}
-            <TableRow>
-              <TableCell>
-                <IconButton size="small" onClick={this.OnAddCriteriaClick} hidden={!isInEditMode}>
-                  <AddIcon />
-                </IconButton>
-              </TableCell>
-              <TableCell />
-            </TableRow>
-          </TableBody>
-        </Table>
-        <Button
-          variant="contained"
-          className={classes.button}
-          onClick={this.OnEditCriteriaClick}
-          hidden={isInEditMode}
-          disabled={classroomId === ''}
-        >
-          Edit Criteria
-        </Button>
-        <Button
-          variant="contained"
-          className={classes.button}
-          onClick={this.OnEditCriteriaCancel}
-          hidden={!isInEditMode}
-        >
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          className={classes.button}
-          onClick={this.OnEditCriteriaSave}
-          hidden={!isInEditMode}
-        >
-          Save
-        </Button>
-      </Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>LABEL</TableCell>
+            <TableCell>DESCRIPTION</TableCell>
+            <TableCell>{IsInEditMode ? 'DELETE' : ''}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Criteria.map(crit => (
+            IsInEditMode ? (
+              <TableRow key={crit.id}>
+                <TableCell>
+                  <TextField
+                    value={crit.label}
+                    placeholder="Label"
+                    onChange={e => UpdateField(crit.id, 'label', e.target.value)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    value={crit.description}
+                    placeholder="Description"
+                    style={{ width: '20em' }}
+                    onChange={e => UpdateField(crit.id, 'description', e.target.value)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <IconButton size="small" onClick={() => OnDeleteCriteriaClick(crit.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ) : (
+              <TableRow key={crit.id}>
+                <TableCell>{crit.label}</TableCell>
+                <TableCell>{crit.description}</TableCell>
+              </TableRow>
+            )
+          ))}
+        </TableBody>
+      </Table>
     );
   }
 }

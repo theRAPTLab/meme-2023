89769c1baa1398297b0cae6f4edc7fee3cb0390a
Dave 'Sri' Seah <david@davidseah.com>
2019-07-18 14:57:52 -0400

    dev-add-urloki: update meme.js to show current git branch

--- meme.js
+++ meme.js
@@ -36,18 +36,26 @@ const { TERM } = MTERM;
 const pathBits = path.parse(argv._[0]);
 const param1 = argv._[1];
 
-const P_SCRIPT = `${TERM.Bright}${pathBits.name.toUpperCase()}${TERM.Reset}`;
+const PR = `${TERM.Bright}${pathBits.name.toUpperCase()}${TERM.Reset}`;
 const P_ERR = `${TERM.Bright}${TERM.BgRed}!ERROR!${TERM.Reset}`;
 
 const PATH_WDS = `./node_modules/webpack-dev-server/bin`;
+const { error, stdout } = shell.exec('git symbolic-ref --short -q HEAD', { silent: true });
+
 switch (param1) {
   case 'dev':
     //    "dev": "echo '\n*** USING WEBPACK HOT DEV SERVER' && webpack-dev-server  --mode development --inline --hot --host 0.0.0.0 --config=./src/config/webpack.webapp.config.js --env.HMR_MODE='wds'",
     console.log(`\n`);
-    console.log(`${P_SCRIPT}: running development server`);
-    console.log(`${P_SCRIPT}: ${TERM.FgYellow}GO TO ONE OF THESE URLS in CHROME WEB BROWSER${TERM.Reset}`);
-    console.log(`${P_SCRIPT}: MAINAPP - http://localhost:3000`);
-    console.log(`${P_SCRIPT}: CLIENTS - http://${ip.address()}:3000`);
+    console.log(`${PR}: running development server`);
+    // git branch information
+    if (error) console.log(`${PR}: ON ${TERM.FgYellow}<DETACHED>${TERM.Reset} BRANCH`);
+    if (stdout) console.log(`${PR}: ON BRANCH ${TERM.FgYellow}${stdout.trim()}${TERM.Reset}`);
+    console.log(`---`);
+    console.log(
+      `${PR}: ${TERM.FgYellow}GO TO ONE OF THESE URLS in CHROME WEB BROWSER${TERM.Reset}`
+    );
+    console.log(`${PR}: MAINAPP - http://localhost:3000`);
+    console.log(`${PR}: CLIENTS - http://${ip.address()}:3000`);
     console.log(`---\n`);
     shell.exec(
       `${PATH_WDS}/webpack-dev-server.js --mode development --inline --hot --host 0.0.0.0 --config=./src/config/webpack.webapp.config.js --env.HMR_MODE='wds'`
@@ -70,7 +78,7 @@ switch (param1) {
         --overwrite
         --app-bundle-id com.davidseah.inquirium.meme
     */
-    console.log(`${P_SCRIPT}\n- ${P_ERR} unimplemented command '${param1}'\n`);
+    console.log(`${PR}\n- ${P_ERR} unimplemented command '${param1}'\n`);
     break;
   case 'appsign':
     /*
@@ -78,33 +86,35 @@ switch (param1) {
         --platform=darwin
         --type=distribution
     */
-    console.log(`${P_SCRIPT}\n- ${P_ERR} unimplemented command '${param1}'\n`);
+    console.log(`${PR}\n- ${P_ERR} unimplemented command '${param1}'\n`);
     break;
   default:
-    console.log(`${P_SCRIPT}\n- ${P_ERR} unknown command '${param1}'\n`);
+    console.log(`${PR}\n- ${P_ERR} unknown command '${param1}'\n`);
 }
 
 function f_DocServe(opt) {
   const loc = `${TERM.FgYellow}localhost:4001${TERM.Reset}`;
 
-  console.log(`${P_SCRIPT}: Point your browser to "${loc}" to read JSDoc-generate documentation.`);
+  console.log(`${PR}: Point your browser to "${loc}" to read JSDoc-generate documentation.`);
   console.log(
-    `${P_SCRIPT}: You can edit source and the documentation will live-update (browser refresh required).`
+    `${PR}: You can edit source and the documentation will live-update (browser refresh required).`
   );
-  console.log(`${P_SCRIPT}: When you're done, type CTRL-C to stop the documentation server`);
-  shell.exec(`npx documentation serve --config meme-documentation.yml --watch ./src/app-web/web-index.js`);
+  console.log(`${PR}: When you're done, type CTRL-C to stop the documentation server`);
+  shell.exec(
+    `npx documentation serve --config meme-documentation.yml --watch ./src/app-web/web-index.js`
+  );
 }
 
 function f_Clean(opt) {
-  console.log(`${P_SCRIPT}: removing dist/ and built/ directories...`);
+  console.log(`${PR}: removing dist/ and built/ directories...`);
   shell.rm('-rf', 'dist', 'built');
   if (opt.all) {
-    console.log(`${P_SCRIPT}: also cleaning node_modules`);
+    console.log(`${PR}: also cleaning node_modules`);
     shell.rm('-rf', 'node_modules');
   }
-  console.log(`${P_SCRIPT}: directories removed!`);
+  console.log(`${PR}: directories removed!`);
   if (opt.all)
     console.log(
-      `${P_SCRIPT}: Make sure to use ${TERM.FgYellow}npm ci${TERM.Reset} to reinstall packages!!!`
+      `${PR}: Make sure to use ${TERM.FgYellow}npm ci${TERM.Reset} to reinstall packages!!!`
     );
 }

6c93ced6187191a827799f0a6a321835657763ad
Ben Loh <ben@inquirium.net>
2019-08-09 21:57:42 -0700

    dev-bl/sticky-refine: Close sticky when clicking outside if user is not currently editing.

--- src/app-web/components/StickyNote.jsx
+++ src/app-web/components/StickyNote.jsx
@@ -15,6 +15,7 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
@@ -48,6 +49,7 @@ class StickyNote extends React.Component {
     this.OnStartEdit = this.OnStartEdit.bind(this);
     this.OnUpdateComment = this.OnUpdateComment.bind(this);
     this.OnCloseClick = this.OnCloseClick.bind(this);
+    this.OnClickAwawy = this.OnClickAwawy.bind(this);
 
     this.state = {
       isHidden: true,
@@ -143,47 +145,59 @@ class StickyNote extends React.Component {
     this.DoCloseSticky();
   }
 
+  OnClickAwawy() {
+    if (!this.state.isHidden && !this.state.isBeingEdited) {
+      this.OnCloseClick();
+    } else {
+      // don't do anything if the user is still editing comment
+    }
+  }
+
   render() {
     const { classes } = this.props;
     const { comments, isHidden, isBeingEdited, top, left } = this.state;
 
     return (
-      <Paper className={classes.stickynotePaper} hidden={isHidden} style={{ top: top, left: left }}>
-        <IconButton
-          size="small"
-          style={{ position: 'absolute', right: '-25px', top: '-25px' }}
-          onClick={this.OnCloseClick}
-        >
-          <CloseIcon />
-        </IconButton>
-        {comments.map(comment => {
-          return (
-            <StickyNoteCard
-              comment={comment}
-              key={comment.id}
-              onStartEdit={this.OnStartEdit}
-              onUpdateComment={this.OnUpdateComment}
-            />
-          );
-        })}
-        <Button
-          size="small"
-          style={{ margin: '5px' }}
-          variant="outlined"
-          hidden={isBeingEdited}
-          onClick={this.OnReplyClick}
-        >
-          Reply
-        </Button>
-        <Button
-          size="small"
-          style={{ float: 'right', margin: '5px' }}
-          variant="outlined"
-          onClick={this.OnCloseClick}
-        >
-          <CloseIcon /> Close
-        </Button>
-      </Paper>
+      <div>
+        <ClickAwayListener onClickAway={this.OnClickAwawy}>
+          <Paper className={classes.stickynotePaper} hidden={isHidden} style={{ top: top, left: left }}>
+            <IconButton
+              size="small"
+              style={{ position: 'absolute', right: '-25px', top: '-25px' }}
+              onClick={this.OnCloseClick}
+            >
+              <CloseIcon />
+            </IconButton>
+            {comments.map(comment => {
+              return (
+                <StickyNoteCard
+                  comment={comment}
+                  key={comment.id}
+                  onStartEdit={this.OnStartEdit}
+                  onUpdateComment={this.OnUpdateComment}
+                />
+              );
+            })}
+            <Button
+              size="small"
+              style={{ margin: '5px' }}
+              variant="outlined"
+              hidden={isBeingEdited}
+              onClick={this.OnReplyClick}
+            >
+              Reply
+            </Button>
+            <Button
+              size="small"
+              style={{ float: 'right', margin: '5px' }}
+              variant="outlined"
+              onClick={this.OnCloseClick}
+            >
+              <CloseIcon /> Close
+            </Button>
+          </Paper>
+        </ClickAwayListener>
+      </div>
     );
   }
 }

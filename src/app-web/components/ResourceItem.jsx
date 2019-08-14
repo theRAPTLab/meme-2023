033aca55388f3c3b448b81c57570b79c20769fe4
Ben Loh <ben@inquirium.net>
2019-08-14 16:12:00 -0700

    dev-bl/misc-items: Clean up ResourceItem

--- src/app-web/components/ResourceItem.jsx
+++ src/app-web/components/ResourceItem.jsx
@@ -5,19 +5,16 @@
 /// LIBRARIES /////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import React from 'react';
+import PropTypes from 'prop-types';
 import ClassNames from 'classnames';
 // Material UI Elements
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
-import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
 import DescriptionIcon from '@material-ui/icons/Description';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ImageIcon from '@material-ui/icons/Image';
@@ -159,6 +156,15 @@ class ResourceItem extends React.Component {
     );
   }
 }
+
+ResourceItem.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  classes: PropTypes.object
+};
+
+ResourceItem.defaultProps = {
+  classes: {}
+};
 
 /// EXPORT REACT COMPONENT ////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

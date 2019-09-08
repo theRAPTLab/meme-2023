61ac0edcad262526efb8e99e81fb51ba09d05d7b
Ben Loh <ben@inquirium.net>
2019-09-07 22:47:24 -0700

    dev-bl/ratings-per-class: Add AdmRatingsView component.

---
+++ src/app-web/views/ViewAdmin/components/AdmRatingsView.jsx
@@ -0,0 +1,185 @@
+/*///////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+Ratings View
+
+Unlike the other components, which send all data updates directly to ADM,
+Ratings are editted locally first, and the whole set of changes is sent
+to ADM after the user clicks "Save".  This is necessary to let users "Cancel"
+out of a criteria edit to revert to the previous state.
+
+We also do things this way so that you can edit all of the items at the same
+time rather than having to individually select and save each edit.
+
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
+
+/// LIBRARIES /////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+// Material UI Theming
+import { withStyles } from '@material-ui/core/styles';
+
+/// COMPONENTS ////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import MEMEStyles from '../../../components/MEMEStyles';
+import UR from '../../../../system/ursys';
+import ADM from '../../../modules/adm-data';
+import RatingsList from '../../../components/RatingsList';
+
+/// DECLARATIONS //////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const DBG = false;
+const PKG = 'AdminRatingsView';
+
+/// CLASS DECLARATION /////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class RatingsView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.DoClassroomSelect = this.DoClassroomSelect.bind(this);
+    this.DoLoadRatings = this.DoLoadRatings.bind(this);
+    this.OnEditClick = this.OnEditClick.bind(this);
+    this.OnSave = this.OnSave.bind(this);
+    this.OnCancel = this.OnCancel.bind(this);
+    this.DoClose = this.DoClose.bind(this);
+    this.DoUpdateField = this.DoUpdateField.bind(this);
+
+    this.state = {
+      ratingsDef: [],
+      origRatingsDef: [],
+      isInEditMode: false,
+      classroomId: ''
+    };
+
+    UR.Subscribe('CLASSROOM_SELECT', this.DoClassroomSelect);
+  }
+
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
+  DoClassroomSelect(data) {
+    this.setState({
+      classroomId: data.classroomId
+    }, () => {
+      this.DoLoadRatings();
+    });
+  }
+  
+  DoLoadRatings() {
+    const ratingsDef = ADM.GetRatingsDefinition(this.state.classroomId);
+    const origRatingsDef = JSON.parse(JSON.stringify(ratingsDef)); // deep clone
+    this.setState({
+      ratingsDef,
+      origRatingsDef
+    });
+  }
+
+  OnEditClick() {
+    this.DoLoadRatings();
+    this.setState({ isInEditMode: true });
+  }
+
+  OnSave(e) {
+    ADM.UpdateRatingsDefinitions(this.state.classroomId, this.state.ratingsDef);
+    this.DoClose();
+  }
+
+  OnCancel() {
+    // Restore original values.
+    this.setState(state => {
+      return { ratingsDef: state.origRatingsDef }
+    }, () => {
+      this.DoClose();
+    });
+  }
+
+  DoClose() {
+    this.setState({
+      isInEditMode: false
+    });
+  }
+
+  DoUpdateField(rating, label) {
+    // Save the changes locally first
+    // Store the whole object when "Save" is presssed.
+    this.setState(state => {
+      let ratingsDef = state.ratingsDef;
+     
+      const index = ratingsDef.findIndex(item => rating === item.rating);
+      ratingsDef[index].label = label;
+      
+      return { ratingsDef };
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { ratingsDef, isInEditMode, classroomId } = this.state;
+
+    return (
+      <Paper className={classes.admPaper}>
+        <InputLabel>RATINGS DEFINITIONS</InputLabel>
+        <Dialog open={isInEditMode}>
+          <DialogTitle>Edit Ratings Definitions</DialogTitle>
+          <DialogContent>
+            <RatingsList
+              RatingsDef={ratingsDef}
+              Mode={isInEditMode ? 'edit' : 'inactive'}
+              UpdateField={this.DoUpdateField}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              className={classes.button}
+              onClick={this.OnCancel}
+              hidden={!isInEditMode}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              className={classes.button}
+              onClick={this.OnSave}
+              hidden={!isInEditMode}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <RatingsList RatingsDef={ratingsDef} Mode="inactive" />
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={this.OnEditClick}
+          hidden={isInEditMode}
+          disabled={classroomId === ''}
+        >
+          Edit Ratings
+        </Button>
+      </Paper>
+    );
+  }
+}
+
+RatingsView.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  classes: PropTypes.object
+};
+
+RatingsView.defaultProps = {
+  classes: {}
+};
+
+/// EXPORT REACT COMPONENT ////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export default withStyles(MEMEStyles)(RatingsView);

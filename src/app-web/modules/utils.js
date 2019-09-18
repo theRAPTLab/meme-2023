0c98cbc3b7b62bd6c257d067874035e2a93f16ab
Dave 'Sri' Seah <david@davidseah.com>
2019-09-18 16:35:19 -0400

    dev-ds-ur-data: fix message name

--- src/app-web/modules/utils.js
+++ src/app-web/modules/utils.js
@@ -10,9 +10,9 @@ const UTILS = {};
  * Researcher Logs
  *
  * This will write events out to the server runtime logs at '../../runtime/logs';
- * 
+ *
  * Calls are documented here: https://docs.google.com/spreadsheets/d/1EjsoXLeaWU-lvtd2addln6gftcqQ4vgzt7Cw9ADl7rw/edit#gid=0
- * 
+ *
  */
 UTILS.RLog = (event, params) => {
   const cleanedParams = params || '';
@@ -21,7 +21,7 @@ UTILS.RLog = (event, params) => {
   const modelId = SESSION.GetSelectedModelId();
   const modelName = SESSION.GetModelTitle(modelId);
   const items = [username, group, modelId, modelName, cleanedParams];
-  UR.NetPublish('SRV_LOG_EVENT', { event, items });
+  UR.NetPublish('NET:SRV_LOG_EVENT', { event, items });
 };
 
 /// EXPORTS ///////////////////////////////////////////////////////////////////

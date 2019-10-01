96ba57ebbeffb85b3161d2e188b709430d9bc170
Ben Loh <ben@inquirium.net>
2019-10-01 13:05:35 -0700

    dev-bl/panzoom: Temporary hack to allow login as Bob

--- src/system/datasets/meme/groups.db.js
+++ src/system/datasets/meme/groups.db.js
@@ -1,5 +1,5 @@
 module.exports = [
-  { id: 1, name: 'Blue', students: ['Bob', 'Bessie', 'Bill'], classroomId: 1 },
+  { id: 1, name: 'Blue', students: ['BOB', 'Bessie', 'Bill'], classroomId: 1 },
   { id: 2, name: 'Green', students: ['Ginger', 'Gail', 'Greg'], classroomId: 1 },
   { id: 3, name: 'Red', students: ['Rob', 'Reese', 'Randy'], classroomId: 1 },
   { id: 4, name: 'Purple', students: ['Peter', 'Paul', 'Penelope'], classroomId: 1 },

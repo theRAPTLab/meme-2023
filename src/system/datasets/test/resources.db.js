839a3ed0ddc31ce88ea3e3d35296505c4faeb641
Morgan VIckery <moravick@iu.edu>
2020-02-06 09:29:47 -0500

    updated resource/model loki files for IU implementation - Feb 6th

--- src/system/datasets/test/resources.db.js
+++ src/system/datasets/test/resources.db.js
@@ -2,100 +2,173 @@ module.exports = [
   {
     id: 1,
     referenceLabel: '1',
-    label: 'Temperature Report',
-    notes: 'temperature, pond conditions',
+    label: 'L1 Dissolved Air.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_1_temp.pdf',
+    url: '../static/dlc/L1 Dissolved Air.pdf',
     links: 0
   },
   {
     id: 2,
     referenceLabel: '2',
-    label: 'Temperature Report 2',
-    notes: 'temperature, pond conditions',
+    label: 'L1 Pond Chlorophyll.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_2_temp.pdf',
+    url: '../static/dlc/L1 Pond Chlorophyll.pdf',
     links: 0
   },
   {
     id: 3,
     referenceLabel: '3',
-    label: 'Necropsy Data',
-    notes: 'necropsy data, dead fish, disease, healthy',
+    label: 'L1 Veterinary Report.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_3_necropsy_data.pdf',
+    url: '../static/dlc/L1 Veterinary Report.pdf',
     links: 0
   },
   {
     id: 4,
     referenceLabel: '4',
-    label: 'Water Quality Report',
-    notes: 'water quality, local pond',
+    label: 'L2 Why are fish suffocating ES V5 Algae from a Pond.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_4_water_quality.pdf',
+    url: '../static/dlc/L2 Why are fish suffocating ES V5 Algae from a Pond.pdf',
     links: 0
   },
   {
     id: 5,
     referenceLabel: '5',
-    label: 'Oxygen report',
-    notes: 'oxygen concentration, fish survival',
+    label: 'L2 Why are fish suffocating ES V5 Algae on Fish.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_5_oxygen.pdf',
+    url: '../static/dlc/L2 Why are fish suffocating ES V5 Algae on Fish.pdf',
     links: 0
   },
   {
     id: 6,
     referenceLabel: '6',
-    label: 'Oxygen report',
-    notes: 'oxygen concentration, fish survival',
+    label: 'L2 Why are fish suffocating ES V5 Leaves from a Park.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_6_oxygen.pdf',
+    url: '../static/dlc/L2 Why are fish suffocating ES V5 Leaves from a Park.pdf',
     links: 0
   },
   {
     id: 7,
     referenceLabel: '7',
-    label: 'Macroscopic Pond Simultion',
-    notes: 'decomposers, fish die, dead matter, macropscopic',
-    type: 'simulation',
-    url: '../static/dlc/resource_7_pond_macro.html',
+    label: 'L3 Station 1b and 1c.pdf',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L3 Station 1b and 1c.pdf',
     links: 0
   },
   {
     id: 8,
     referenceLabel: '8',
-    label: 'Oxygen report',
-    notes: 'fish, algae, gills, body',
+    label: 'L3 Station 3.html',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_8_algae_on_fish.pdf',
+    url: '../static/dlc/L3 Station 3.html',
     links: 0
   },
   {
     id: 9,
     referenceLabel: '9',
-    label: 'Analysis of dead matter - pond',
-    notes: 'decomposers, dead matter, algae',
+    label: 'L3 Station 3.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_9_analysis_dead_matter_pond.pdf',
+    url: '../static/dlc/L3 Station 3.pdf',
     links: 0
   },
   {
     id: 10,
     referenceLabel: '10',
-    label: 'Analysis of dead matter - park',
-    notes: 'decomposers, dead matter, algae',
+    label: 'L4 Resource 1 Amount of Sunlight at Passion Puddle.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_9_analysis_dead_matter_park.pdf',
+    url: '../static/dlc/L4 Resource 1 Amount of Sunlight at Passion Puddle.pdf',
     links: 0
   },
   {
     id: 11,
     referenceLabel: '11',
-    label: 'Decomposition (video)',
-    notes: 'decomposition',
+    label: 'L4 Resource 1 Light and Air Quality.pdf',
+    notes: '',
     type: 'report',
-    url: '../static/dlc/resource_11_video_of_decomp.html',
+    url: '../static/dlc/L4 Resource 1 Light and Air Quality.pdf',
     links: 0
-  }
+  },
+  {
+    id: 12,
+    referenceLabel: '12',
+    label: 'L4 Resource 2 Plants in Sunlight.pdf',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L4 Resource 2 Plants in Sunlight.pdf',
+    links: 0
+  },
+  {
+    id: 13,
+    referenceLabel: '13',
+    label: 'L4 Resource 3 Fertilizer and Plants.pdf',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L4 Resource 3 Fertilizer and Plants.pdf',
+    links: 0
+  },
+  {
+    id: 14,
+    referenceLabel: '14',
+    label: 'L4 Resource 4 Air in Aquariums.html',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L4 Resource 4 Air in Aquariums.html',
+    links: 0
+  },
+  {
+    id: 15,
+    referenceLabel: '15',
+    label: 'L4 Resource 4 Table.png',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L4 Resource 4 Table.png',
+    links: 0
+  },
+  {
+    id: 16,
+    referenceLabel: '16',
+    label: 'L4 Resource 5 Reading Fertilizer Labels.pdf',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L4 Resource 5 Reading Fertilizer Labels.pdf',
+    links: 0
+  },
+  {
+    id: 17,
+    referenceLabel: '17',
+    label: 'L4 Resource 6 Lake Erie Algal Blooms.pdf',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/L4 Resource 6 Lake Erie Algal Blooms.pdf',
+    links: 0
+  },
+  {
+    id: 18,
+    referenceLabel: '18',
+    label: 'Macroscopic Pond Simulation.html',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/Macroscopic Pond Simulation Simple.html',
+    links: 0
+  },
+  {
+    id: 19,
+    referenceLabel: '19',
+    label: 'Microscopic Sim.html',
+    notes: '',
+    type: 'report',
+    url: '../static/dlc/Microscopic Sim.html',
+    links: 0
+  },
+
 ];

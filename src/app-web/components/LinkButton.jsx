bc49a408d2cf840c289bc6b8e75a0c7d56b52371
Ben Loh <ben@inquirium.net>
2020-04-30 17:31:13 -0700

    misc: Restore padding to Link buttons

--- src/app-web/components/LinkButton.jsx
+++ src/app-web/components/LinkButton.jsx
@@ -69,7 +69,7 @@ const { COLOR } = DEFAULTS;
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const LButton = withStyles(theme => ({
   root: {
-    padding: '0 7px',
+    padding: '2px 7px',
     lineHeight: '1.2em',
     '&$disabled': {
       color: 'rgba(0,0,0,0.3)'

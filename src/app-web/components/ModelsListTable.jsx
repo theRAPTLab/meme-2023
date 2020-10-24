94df7d92b18a6d2fa65f702f4dec5d9dc9e36d43
Ben Loh <ben@inquirium.net>
2020-10-24 13:17:22 -0700

    sortTable: Add sortable models list tables.

--- src/app-web/components/ModelsListTable.jsx
+++ src/app-web/components/ModelsListTable.jsx
@@ -16,6 +16,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 // Material UI Theming
 import DeleteIcon from '@material-ui/icons/DeleteOutlined';
 import { withStyles } from '@material-ui/core/styles';
@@ -44,38 +45,110 @@ function HumanDate(timestamp) {
   return `${datestring} ${timestring}`;
 }
 
+function DescendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function GetComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => DescendingComparator(a, b, orderBy)
+    : (a, b) => -DescendingComparator(a, b, orderBy);
+}
+
+function Sort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
 class ModelsListTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      order: 'desc',
+      orderBy: 'dateModified'
+    };
+    this.OnSortClick = this.OnSortClick.bind(this);
+  }
+
   componentDidMount() {}
 
   componentWillUnmount() {}
+
+  OnSortClick(id) {
+    this.setState(state => ({
+      order: state.orderBy === id && state.order === 'asc' ? 'desc' : 'asc',
+      orderBy: id
+    }));
+  }
 
   render() {
     const {
       classes,
       models,
       isAdmin,
+      showGroup,
       OnModelSelect,
       OnModelMove,
       OnModelClone,
       OnModelDelete
     } = this.props;
+    const { orderBy, order } = this.state;
+
     const showAdminOnlyView = SESSION.IsTeacher() || isAdmin;
     const isLoggedIn = SESSION.LoggedInName() !== undefined;
+
+    const headCells = [{ id: 'title', label: 'TITLE' }];
+    if (showAdminOnlyView) {
+      headCells.push({ id: 'groupLabel', label: 'CLASSROOM:GROUP' });
+    } else if (showGroup) {
+      headCells.push({ id: 'groupLabel', label: 'GROUP' });
+    }
+    headCells.push(
+      { id: 'dateModified', label: 'UPDATED' },
+      { id: 'dateCreated', label: 'CREATED' },
+      { id: 'actions', label: '' }
+    );
+
+    const modelsWithGroupLabels = models.map(m => {
+      // in showAdminOnlyView, also show Classrooms
+      return Object.assign(m, {
+        groupLabel: showAdminOnlyView
+          ? `${ADM.GetClassroomNameByGroup(m.groupId)}:${ADM.GetGroupName(m.groupId)}`
+          : ADM.GetGroupName(m.groupId)
+      });
+    });
 
     return (
       <Paper className={classes.admPaper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>TITLE</TableCell>
-              <TableCell>{showAdminOnlyView ? 'CLASSROOM: ' : ''}GROUP</TableCell>
-              <TableCell>UPDATED</TableCell>
-              <TableCell>CREATED</TableCell>
-              <TableCell> </TableCell>
+              {headCells.map(c => (
+                <TableCell key={c.id}>
+                  <TableSortLabel
+                    active={orderBy === c.id}
+                    direction={orderBy === c.id ? order : 'asc'}
+                    onClick={() => this.OnSortClick(c.id)}
+                  >
+                    {c.label}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {models.map(model => (
+            {Sort(modelsWithGroupLabels, GetComparator(order, orderBy)).map(model => (
               <TableRow key={model.id}>
                 <TableCell>
                   {isLoggedIn ? (
@@ -86,10 +159,7 @@ class ModelsListTable extends React.Component {
                     model.title
                   )}
                 </TableCell>
-                <TableCell>
-                  {showAdminOnlyView ? `${ADM.GetClassroomNameByGroup(model.groupId)}: ` : ''}
-                  {ADM.GetGroupName(model.groupId)}
-                </TableCell>
+                {(showAdminOnlyView || showGroup) && <TableCell>{model.groupLabel}</TableCell>}
                 <TableCell>{HumanDate(model.dateModified)}</TableCell>
                 <TableCell>{HumanDate(model.dateCreated)}</TableCell>
                 <TableCell>
@@ -122,6 +192,7 @@ ModelsListTable.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   models: PropTypes.array,
   isAdmin: PropTypes.bool,
+  showGroup: PropTypes.bool,
   OnModelSelect: PropTypes.func,
   OnModelMove: PropTypes.func,
   OnModelClone: PropTypes.func,
@@ -132,6 +203,7 @@ ModelsListTable.defaultProps = {
   classes: {},
   models: [],
   isAdmin: false,
+  showGroup: false,
   OnModelSelect: () => {
     console.error('Missing OnModeSelect handler');
   },

fabd50856196162e7fa3ae560fb4b01f306c35e2
DSri Seah <dsri@Sris-MacBookPro-16.local>
2024-06-18 12:29:20 -0400

    build-config-fixes: rewrite eslintrc.js to conform to eslint module format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
-module.exports = {
+/*///////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+  eslint configuration for MEME
+  updated 2024-06-18
+
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
+
+/// CONFIG BLOCKS /////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/** base eslint configuration */
+const CONFIG = {
   'env': {
     'browser': true,
     'commonjs': true,
@@ -19,39 +29,54 @@ module.exports = {
       'jsx': true
     },
     'sourceType': 'module'
-  },
-  'rules': {
-    'prettier/prettier': ['warn'],
-    'no-console': 'off',
-    'no-debugger': 'warn',
-    'no-alert': 'warn',
-    'spaced-comment': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      {
-        'extensions': ['.js', '.jsx']
-      }
-    ],
-    'no-unused-vars': 'off',
-    'arrow-body-style': 'off',
-    'react/destructuring-assignment': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'camelcase': 'off',
-    'no-use-before-define': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        'devDependencies': true
-      }
-    ],
-    'no-underscore-dangle': 'off',
-    'no-plusplus': 'off',
-    'prefer-const': 'off',
-    'prefer-destructuring': 'off',
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'warn',
-    'react/prop-types': 'warn',
-    'react/no-unescaped-entities': 'off'
-  },
-  'settings': {}
+  }
+};
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/** relaxations of rules that we don't mind */
+const INQ_RULES = {
+  'prettier/prettier': ['warn'],
+  'no-console': 'off',
+  'no-debugger': 'warn',
+  'no-alert': 'warn',
+  'spaced-comment': 'off',
+  'react/jsx-filename-extension': [
+    1,
+    {
+      'extensions': ['.js', '.jsx']
+    }
+  ],
+  'no-unused-vars': 'off',
+  'arrow-body-style': 'off',
+  'camelcase': 'off',
+  'no-use-before-define': 'off',
+  'import/no-extraneous-dependencies': [
+    'error',
+    {
+      'devDependencies': true
+    }
+  ],
+  'no-underscore-dangle': 'off',
+  'no-plusplus': 'off',
+  'prefer-const': 'off',
+  'prefer-destructuring': 'off',
+  'class-methods-use-this': 'off',
+  'no-param-reassign': 'warn'
+};
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/** react rules that we don't like */
+const REACT_ICKS = {
+  'react/prop-types': 'warn',
+  'react/no-unescaped-entities': 'off',
+  'react/destructuring-assignment': 'off',
+  'react/jsx-one-expression-per-line': 'off'
+};
+
+/// EXPORTS ///////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+module.exports = {
+  ...CONFIG, // base eslint config
+  rules: {
+    ...INQ_RULES, // default inq styles
+    ...REACT_ICKS
+  }
 };

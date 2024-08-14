a54dc32a1c51172373b016599120bc97fbb5f701
benloh <ben@inquirium.net>
2024-08-14 10:54:09 -0700

    de-muiify-evidence: Add sort model list by title.

--- src/app-web/components/WModelsListTable.jsx
+++ src/app-web/components/WModelsListTable.jsx
@@ -124,6 +124,15 @@ class WModelsListTable extends React.Component {
       {
         title: 'TITLE',
         renderer: this.RendererTitle,
+        columnSorting: {
+          compareFunctionFactory: (sortOrder, columnMeta) => {
+            return (a, b) => {
+              if (a.title < b.title) return -1;
+              if (a.title > b.title) return 1;
+              return 0;
+            };
+          }
+        },
         data: 'title'
       },
       {

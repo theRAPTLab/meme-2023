19e2988bb5cc75394b1d1a9511e7e961e0ac84bb
benloh <ben@inquirium.net>
2024-11-16 17:18:16 -0800

    urtable: Add sort indicators and previous sort order memory

--- src/system/table/URTable.jsx
+++ src/system/table/URTable.jsx
@@ -2,38 +2,154 @@
 
 UR Table
 
-Implements a table with resizable columns.
+Implements a table with resizable and sortable columns.
 Emulates the API of Handsontable.
+Used also on https://github.com/netcreateorg/netcreate-itest/
 
+# API - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+## Renderers
+  This emulates Handsontable's renderer concept.
+  * A renderer is a function that takes a value and returns a JSX element
+  * If a column has a renderer, it will be used to render the value in the cell
+
+## Sorters
+  This emulates Handsontable's sorter concept.
+  * A sorter is a function that takes a key, the table data, and the sort order
+  * If a column has a sorter, it will be used to sort the table data
+
+## Column Widths
+  * Column widths can be set in the column definition
+  * Columns without a width will be evenly distributed
+
+
+# User Interaction - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+## Resizing Columns
+  * Columns can be resized by dragging the right edge of the column header
+    The column to the right of the dragged edge will expand or contract
+  * Max resize -- Resizing is done between two columns: the current column,
+  and the column to the right. (We assume the table width is fixed or 100%).
+  To give you simple, but finer control over resizing columns, you can expand or
+  contract the current column and the next column up/down to a minimum size.
+  Once you hit the max size, you need to resize other columns to adjust.
+  (We don't allow you to resize neighboring columns once you've hit the
+  max/min just to simplify the math).
+
+## Sort Order
+  By default the table is unsorted.
+  * Clicking on a column header will sort the table by that column in ascending order
+  * Subsequent clicks will toggle to descending to unsorted and back to ascending
+  * The next sort order is highlighted on hover with a transparent arrow
+  * The column remembers the previous sort order when another column is selected
+    so re-selecting the column will restore the previous sort order
+  * A column can be designated unsortable by setting `sortDisabled` to `true`
+
+
+# PROPS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  `tableData`: array
+      The data to be displayed in the table.
+
+      [
+        { id: 1, label: 'Constantinople', founding: '100 ce' },
+        { id: 2, label: 'Athens', founding: '1000 bce' },
+        { id: 3, label: 'Cairo', founding: '3000 bce' },
+      ]
+
+  `isOpen`: boolean
+      To optimize performance, the column widths are only calculated
+      when it's open.
+
+  `columns`: array
+      Specifications for how each column is to be rendered and sorted.
+
+      Column Definition
+        title: string
+        data: string
+        type: 'text' | 'number' | 'date' | 'markdown' | 'hdate'
+        width: number
+        renderer: (value: any) => JSX.Element
+        sorter: (key: string, tdata: any[], order: number) => any[]
+        sortDisabled: boolean
+
+
+  Example usage:
+
+    const COLUMNDEFS = [
+      {
+        title: 'TITLE',
+        data: 'title',
+        type: 'text',
+        width: 300, // in px
+        renderer: this.RendererTitle,
+        sorter: (key, tdata, order) => {
+          const sortedData = [...tdata].sort((a, b) => {
+            // note `title` is stuffed into `tdata`
+            if (a[key].title < b[key].title) return order;
+            if (a[key].title > b[key].title) return order * -1;
+            return 0;
+          });
+          return sortedData;
+        }
+      },
+      {
+        title: 'UPDATED',
+        data: 'dateModified',
+        type: 'text',
+        width: 300 // in px
+      },
+    ]
+
+    const TABLEDATA = modelsWithGroupLabels.map(model => {
+      return {
+        id: model.id,
+        title: model.title,
+        groupLabel: model.groupLabel,
+        dateModified: HumanDate(model.dateModified),
+        dateCreated: HumanDate(model.dateCreated)
+      };
+    });
 
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
 
 /// LIBRARIES /////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import React, { useState, useEffect, useRef } from 'react';
+import './URTable.css';
 // import HDATE from 'system/util/hdate';
 
 /// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const DBG = false;
 
+const SORTORDER = new Map();
+SORTORDER.set(0, '▲▼');
+SORTORDER.set(1, '▲');
+SORTORDER.set(-1, '▼');
+
 /// FUNCTIONAL COMPONENT DECLARATION //////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function URTable({ isOpen, data, columns }) {
-  const [tabledata, setTableData] = useState([]);
-  const [columndefs, setColumnDefs] = useState([]);
-  const [columnWidths, setColumnWidths] = useState([]);
-  const [sortColumnIdx, setSortColumnIdx] = useState(0);
-  const [sortOrder, setSortOrder] = useState(1);
+  const [_tabledata, setTableData] = useState([]);
+  const [_columndefs, setColumnDefs] = useState([]);
+  const [_columnWidths, setColumnWidths] = useState([]);
+  const [_sortColumnIdx, setSortColumnIdx] = useState(0);
+  const [_sortOrder, setSortOrder] = useState(0);
+  const [_previousColSortOrder, setPreviousColSortOrder] = useState({});
 
-  const tableRef = useRef(null);
-  const resizeRef = useRef(null);
+  const ref_Table = useRef(null);
+  const ref_Resize = useRef(null);
 
   /// USE EFFECT //////////////////////////////////////////////////////////////
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   /// Init table data
   useEffect(() => {
     setTableData(data);
+
+    // default to ascending when a column is first clicked
+    const defaultSortOrders = {};
+    data.forEach((item, idx) => (defaultSortOrders[idx] = 1));
+    setPreviousColSortOrder(defaultSortOrders);
   }, []);
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   /// Calculate Initial Column Widths
@@ -61,22 +177,22 @@ function URTable({ isOpen, data, columns }) {
   useEffect(() => {
     // Sort table data
     m_ExecuteSorter(data);
-  }, [sortColumnIdx, sortOrder]);
+  }, [_sortColumnIdx, _sortOrder, _previousColSortOrder]);
 
   /// UTILITIES ///////////////////////////////////////////////////////////////
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   const u_CalculateColumnWidths = () => {
     // if table is not drawn yet, skip
-    if (tableRef.current.clientWidth < 1) return;
+    if (ref_Table.current.clientWidth < 1) return;
 
     // if it's already set, don't recalculate
-    if (columnWidths.length > 0 && tableRef.current.clientWidth > 0) {
+    if (_columnWidths.length > 0 && ref_Table.current.clientWidth > 0) {
       return;
     }
 
     const definedColWidths = columns.filter(col => col.width).map(col => col.width);
     const definedColWidthSum = definedColWidths.reduce((a, b) => a + b, 0);
-    const remainingWidth = tableRef.current.clientWidth - definedColWidthSum;
+    const remainingWidth = ref_Table.current.clientWidth - definedColWidthSum;
     const colWidths = columns.map(
       col => col.width || remainingWidth / (columns.length - definedColWidths.length)
     );
@@ -88,20 +204,20 @@ function URTable({ isOpen, data, columns }) {
   const ui_MouseDown = (event, index) => {
     event.preventDefault();
     event.stopPropagation();
-    resizeRef.current = {
+    ref_Resize.current = {
       index,
       startX: event.clientX,
-      startWidth: columnWidths[index],
-      nextStartWidth: columnWidths[index + 1],
-      maxCombinedWidth: columnWidths[index] + columnWidths[index + 1] - 50
+      startWidth: _columnWidths[index],
+      nextStartWidth: _columnWidths[index + 1],
+      maxCombinedWidth: _columnWidths[index] + _columnWidths[index + 1] - 50
     };
   };
   const ui_MouseMove = event => {
-    if (resizeRef.current !== null) {
+    if (ref_Resize.current !== null) {
       const { index, startX, startWidth, nextStartWidth, maxCombinedWidth } =
-        resizeRef.current;
+        ref_Resize.current;
       const delta = event.clientX - startX;
-      const newWidths = [...columnWidths];
+      const newWidths = [..._columnWidths];
       newWidths[index] = Math.min(Math.max(50, startWidth + delta), maxCombinedWidth); // Minimum width set to 50px
       newWidths[index + 1] = Math.min(
         Math.max(50, nextStartWidth - delta),
@@ -111,16 +227,35 @@ function URTable({ isOpen, data, columns }) {
     }
   };
   const ui_MouseUp = () => {
-    resizeRef.current = null; // Reset on mouse up
+    ref_Resize.current = null; // Reset on mouse up
   };
 
   /// CLICK HANDLERS //////////////////////////////////////////////////////////
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  function ui_ClickSorter(event, index) {
+  function ui_SetSelectedColumn(event, index) {
     event.preventDefault();
     event.stopPropagation();
+    if (_columndefs[index].sortDisabled) return; // If sort is disabled, do nothing
+
+    if (_sortColumnIdx === index) {
+      // if already selected, toggle the sort order
+      let newSortOrder;
+      if (_sortOrder === 0) newSortOrder = 1;
+      else if (_sortOrder > 0) newSortOrder = -1;
+      else newSortOrder = 0;
+      setSortOrder(newSortOrder);
+    } else {
+      // otherwise default to the previous order
+      setSortOrder(_previousColSortOrder[index]);
+    }
+
+    // update the previous sort order
+    setPreviousColSortOrder({
+      ..._previousColSortOrder,
+      [_sortColumnIdx]: _sortOrder
+    });
+
     setSortColumnIdx(index);
-    setSortOrder(sortOrder * -1);
   }
 
   /// BUILT-IN SORTERS ////////////////////////////////////////////////////////
@@ -224,66 +359,98 @@ function URTable({ isOpen, data, columns }) {
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   /// Sorts then sets the table data
   function m_ExecuteSorter(tdata) {
-    if (columndefs.length < 1) return [];
+    if (_columndefs.length < 1) return [];
 
-    const customSorter = columndefs[sortColumnIdx].sorter;
-    const key = columndefs[sortColumnIdx].data;
+    const customSorter = _columndefs[_sortColumnIdx].sorter;
+    const key = _columndefs[_sortColumnIdx].data;
     let sortedData = [];
     if (customSorter) {
       if (typeof customSorter !== 'function') throw new Error('Invalid sorter');
-      sortedData = customSorter(key, tdata, sortOrder);
+      sortedData = customSorter(key, tdata, _sortOrder);
     } else {
       // Run built-in sorters
-      switch (columndefs[sortColumnIdx].type) {
+      switch (_columndefs[_sortColumnIdx].type) {
         // case 'hdate':
         //   sortedData = m_SortByHDate(key, tdata, sortOrder);
         //   break;
         case 'markdown':
-          sortedData = m_SortByMarkdown(key, tdata, sortOrder);
+          sortedData = m_SortByMarkdown(key, tdata, _sortOrder);
           break;
         case 'date':
         case 'number':
-          sortedData = m_SortByNumber(key, tdata, sortOrder);
+          sortedData = m_SortByNumber(key, tdata, _sortOrder);
           break;
         case 'text':
         default:
-          sortedData = m_SortByText(key, tdata, sortOrder);
+          sortedData = m_SortByText(key, tdata, _sortOrder);
       }
     }
     setTableData(sortedData);
   }
   /// RENDER //////////////////////////////////////////////////////////////////
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  /**
+   * jsx_SortBtn tracks two states:
+   * if sort is disabled:
+   *  - do not show sort button in header
+   *  - the header is not clickable (no cursor pointer)
+   * if sort is enabled:
+   *  - show sort erder: '▲' or '▼' or '▲▼'
+   *  - show cursor pointer
+   */
+  function jsx_SortBtn(columndef, idx) {
+    const isSelected = _sortColumnIdx === idx;
+    if (columndef.sortDisabled) {
+      return <span className="sortDisabled">-</span>;
+    } else if (isSelected) {
+      return <span className="sortEnabled">{SORTORDER.get(_sortOrder)}</span>;
+    } else {
+      // not selected, so sort order is the previous sort order
+      return (
+        <span className="sortEnabled">
+          {SORTORDER.get(_previousColSortOrder[idx])}
+        </span>
+      );
+    }
+  }
+
+  // show cursor pointer if not sortDisabled
+  // needs to be set at `th` not at `th span`
+
   return (
     <div
       className="URTable"
-      ref={tableRef}
+      ref={ref_Table}
       onMouseMove={ui_MouseMove}
       onMouseUp={ui_MouseUp}
     >
       <table>
         <thead>
           <tr>
-            {columndefs.map((col, idx) => (
+            {_columndefs.map((col, idx) => (
               <th
                 key={idx}
-                className={sortColumnIdx === idx ? 'selected' : ''}
-                width={`${columnWidths[idx]}`}
+                className={_sortColumnIdx === idx ? 'selected' : ''}
+                width={`${_columnWidths[idx]}`}
               >
-                <div onClick={e => ui_ClickSorter(e, idx)}>{col.title}</div>
+                <div onClick={e => ui_SetSelectedColumn(e, idx)}>
+                  {col.title}&nbsp;
+                  {jsx_SortBtn(col, idx)}
+                </div>
                 <div
                   className="resize-handle"
                   onMouseDown={e => ui_MouseDown(e, idx)}
-                  hidden={idx === columndefs.length - 1} // hide last resize handle
+                  hidden={idx === _columndefs.length - 1} // hide last resize handle
                 ></div>
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {tabledata.map((tdata, idx) => (
+          {_tabledata.map((tdata, idx) => (
             <tr key={idx} style={{ opacity: 1 }}>
-              {columndefs.map((col, idx) => (
+              {_columndefs.map((col, idx) => (
                 <td key={idx}>{m_ExecuteRenderer(tdata[col.data], col, idx)}</td>
               ))}
             </tr>

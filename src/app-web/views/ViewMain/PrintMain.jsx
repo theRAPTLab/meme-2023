theRAPTLab/meme-2023

commit 4e5197d05093a8308d4db9013fe9a1ede8f523a7

hack: Show fixed graph width/height for print view.  Needs to be fixed with MUI deprecation. Fixes #123

src/app-web/views/ViewMain/PrintMain.css
@@ -1,0 +1,3 @@
+#app-container {
+  overflow: visible; /* override ViewMEME.css */
+}

src/app-web/views/ViewMain/PrintMain.jsx
@@ -40,6 +40,9 @@ import ASET from '../../modules/adm-settings';
 import DATAMAP from '../../../system/common-datamap';
 import { cssreact, cssdraw, cssalert } from '../../modules/console-styles';
 
+// Override ViewMEME.css
+import './PrintMain.css';
+
 /// CONSTANTS /////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const DBG = false;
@@ -152,11 +155,18 @@ class PrintMain extends React.Component {
     const innerHeight = window.innerHeight - this.toolRect.height;
 
     // debugging: double-refresh issue
-    console.log('%cUpdateDimensions Fired', cssdraw);
+    console.log(`%cUpdateDimensions Fired ${viewWidth} ${viewHeight}`, cssdraw);
+    // HACK: For print view, ignore window height so that the graph will show the full size
+    // REVIEW: The proper solution is to a) remove MUI, b) redo the rendering
     this.setState({
-      viewWidth: Math.min(viewWidth, innerWidth),
-      viewHeight: Math.min(viewHeight, innerHeight)
+      viewWidth: 1024,
+      viewHeight: 1024
     });
+    // Orig code:
+    // this.setState({
+    //   viewWidth: Math.min(viewWidth, innerWidth),
+    //   viewHeight: Math.min(viewHeight, innerHeight)
+    // });
   }
 
   OnCloseModel() {

commit 73d9c39c2e61a37ab518cafe7454edd9d1a9c76b

hotfix: Fix overflows on short screens causing scrolling issues. Fixes #127.
Addresses bug introduced by 4e5197d05093a8308d4db9013fe9a1ede8f523a7

src/app-web/views/ViewMain/PrintMain.css
@@ -1,3 +1,0 @@
-#app-container {
-  overflow: visible; /* override ViewMEME.css */
-}

src/app-web/views/ViewMain/PrintMain.jsx
@@ -40,9 +40,6 @@ import ASET from '../../modules/adm-settings';
 import DATAMAP from '../../../system/common-datamap';
 import { cssreact, cssdraw, cssalert } from '../../modules/console-styles';
 
-// Override ViewMEME.css
-import './PrintMain.css';
-
 /// CONSTANTS /////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const DBG = false;
@@ -91,6 +88,13 @@ class PrintMain extends React.Component {
   }
 
   componentDidMount() {
+    // Hack to allow print to show full height
+    // Without this, the list of properties is cut off
+    // We can't use PrintMain.css because modifying `#app-container` affects ViewMEME as well
+    // and causes scrolling problems.
+    const appcontainer = document.getElementById('app-container');
+    appcontainer.style.overflow = 'visible';
+
     // if (DBG) console.log(`%ccomponentDidMount()`, cssreact);
     //
     // child components need to know the dimensions
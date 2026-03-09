b9014b32a5fd4102fc5531a711d505bc9c2b0c28
benloh <ben@inquirium.net>
2026-03-09 13:59:36 -0700

    hotfix-circular-parent: Clear model on logout.  Fixes #159

--- src/app-web/modules/adm-data.js
+++ src/app-web/modules/adm-data.js
@@ -784,7 +784,8 @@ ADMData.Logout = () => {
         urs.SESSION_Key = '';
         SESSION.Clear();
         ASET.selectedStudentId = '';
-        ADMData.SelectClassroom('');
+        ASET.selectedModelId = ''; // clear model so DoDataUpdate skips GetRatings
+        ADMData.SelectClassroom(''); // clears selectedUnitId
         UR.Publish('ADM_DATA_UPDATED');
         return rdata;
       }
